564e20153657c7bab71af7fab315f7a487d0c5dd
Danielle Byrne <DanielleByrne421@gmail.com>
2020-07-07 22:14:51 -0400

    adding in post route

--- apiRoutes.js
+++ apiRoutes.js
@@ -4,7 +4,7 @@ const path = require("path");
 const fs = require("fs");
 
 module.exports = function (app) {
-  app.get("/api/notes",  (req, res)=> {
+  app.get("/api/notes", (req, res) => {
     fs.readFile("./db/db.json", "utf8", (err, data) => {
       if (err) {
         return res.send("an error occured");
@@ -13,5 +13,16 @@ module.exports = function (app) {
       res.json(arrayOfNotes);
     });
   });
+  app.post("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      const arrayOfNotes = JSON.parse(data);
+      arrayOfNotes.push(req.body);
+      fs.writeFile("./db/db.json", JSON.stringify(arrayOfNotes), (err) => {
+        if (!err) {
+          console.log("worked");
+        }
+        res.json(arrayOfNotes);
+      });
+    });
+  });
 };
-
