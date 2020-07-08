46855cc5baa7005b5984017a40637a1707d74b6d
Danielle Byrne <DanielleByrne421@gmail.com>
2020-07-07 23:55:15 -0400

    delete route working

--- apiRoutes.js
+++ apiRoutes.js
@@ -17,12 +17,35 @@ module.exports = function (app) {
     fs.readFile("./db/db.json", "utf8", (err, data) => {
       const arrayOfNotes = JSON.parse(data);
       arrayOfNotes.push(req.body);
+      arrayOfNotes.map((obj, i) => (obj.id = ++i));
       fs.writeFile("./db/db.json", JSON.stringify(arrayOfNotes), (err) => {
         if (!err) {
           console.log("worked");
         }
         res.json(arrayOfNotes);
       });
+    });
+  });
+  app.delete("/api/notes/:id", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        return res.send("an error occured");
+      }
+      const arrayOfNotes = JSON.parse(data);
+      const deleteNotes = arrayOfNotes.filter(
+        (user) => user.id != req.params.id
+      );
+      fs.writeFile(
+        "./db/db.json",
+        JSON.stringify(deleteNotes),
+        "utf8",
+        (err, data) => {
+          if (err) {
+            return res.send("an error occured");
+          }
+          res.json(arrayOfNotes);
+        }
+      );
     });
   });
 };
